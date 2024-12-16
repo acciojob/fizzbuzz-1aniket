b0ac5c4829bf57cf22be59b333dccef36528e784
Aniket Bhogawar <39596152+1aniket@users.noreply.github.com>
2024-12-16 21:02:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+
+function fizzBuzz() {
+
+	let ans="";
+
+	for(let i=1;i<=20;i++)
+		{
+			if(i%3==0)
+			{
+				if(i%5==0)
+				{
+					ans+="FizzBuzz";
+				}else
+				{
+					ans+="Fizz";
+				}
+				
+			}else if(i%5==0)
+			{
+				ans+="Buzz";
+			}else
+			{
+				ans+=i;
+			}
+			ans+="\n";
+		}
+	return ans;
+}
+
+alert(fizzBuzz())
